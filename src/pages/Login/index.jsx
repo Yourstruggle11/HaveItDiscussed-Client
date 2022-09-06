55186f6b836e6f22d87acfda2b6f9155bf61f82a
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-07 00:42:13 +0530

    `finalize`: finalize `login` with `popup` anfd `redirects`

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,23 +1,58 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { FirebaseContext } from '../../firebase/firebase'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+    const [isLoading, setIsLoading] = useState(false)
     const { api } = useContext(FirebaseContext)
+
+  const { userInfo } = useSelector((state) => state.userSignin)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/question')
+    }
+  }, [userInfo])
+  
+
+    const notify = () =>
+    toast('You will be logged in few seconds', {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+    })
 
           /* -----------------Google Authentication------------- */
   const googleAuth = async () => {
-    // setIsLoading(true)
+    setIsLoading(true)
     const errMsg = await api.googleSignIn()
     if (errMsg) {
       alert(errMsg)
     } else {
-        alert("login success")
-    //   notify()
+      notify()
     }
-    // setIsLoading(false)
+    setIsLoading(false)
 }
   return (
     <>
+          <ToastContainer
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        // className={classes.socialToastContainer}
+      />
         <div className="bg-blue-400 h-screen w-screen overflow-hidden">
     <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
         <div className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 bg-white sm:mx-0 h-[500px]">
@@ -27,10 +62,16 @@ export default function Login() {
                     <div className="w-full mt-4">
                         <div className="form-horizontal w-3/4 mx-auto" method="POST" action="#">
                             <div className="flex flex-col mt-8">
-                                <button onClick={googleAuth}
+                                {!isLoading ? <button onClick={googleAuth}
                                 type="submit" className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
                                     Login With Google
                                 </button>
+                                :
+                                <button
+                                type="submit" className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
+                                    Loading...
+                                </button>
+                                }
                             </div>
                         </div>
                     </div>
