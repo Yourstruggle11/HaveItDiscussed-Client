38f92a92b09b85843d97a4ebc8eb5d8b3c882e8e
Yourstruggle11 <sensouvik51@gmail.com>
2023-06-21 04:34:03 +0530

    `fix`: Fix `Edit button` `unresponsiveness`

--- src/pages/Profile/components/UserInformation/index.jsx
+++ src/pages/Profile/components/UserInformation/index.jsx
@@ -15,14 +15,14 @@ function UserInformation({
 
     return (
         <div className="text-center mt-12">
-            <div className="flex items-center justify-center">
+            <div className="flex lg:flex-row flex-col items-center justify-center">
                 <h3 className="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">
                     {userProfileData?.name}
                 </h3>
                 {isCurrentUser &&
                     (isEditing ? (
                         <button
-                            className="ml-2 px-4 py-2 text-base font-medium text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            className="ml-2 mb-2 px-4 py-2 text-base font-medium text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                             onClick={() => {
                                 setIsEditing(false)
                                 handleSave()
@@ -32,7 +32,7 @@ function UserInformation({
                         </button>
                     ) : (
                         <button
-                            className="ml-2 px-4 py-2 text-base font-medium text-blue-600 bg-transparent rounded-md hover:bg-blue-50 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            className="ml-2 mb-2 px-4 py-2 text-base font-medium text-blue-600 bg-transparent rounded-md hover:bg-blue-50 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                             onClick={() => setIsEditing(true)}
                         >
                             Edit Profile
