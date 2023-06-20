129c93545b5b10801f9263ad1b216bd72f680b6f
Yourstruggle11 <sensouvik51@gmail.com>
2023-06-21 03:03:52 +0530

    `feat`: Complete connection for `Accept Friend Request` API

--- src/pages/Profile/components/Connection/components/FriendRequestActions/index.jsx
+++ src/pages/Profile/components/Connection/components/FriendRequestActions/index.jsx
@@ -1,11 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { acceptFriendRequest } from '../../../../../../redux/actions/friendsAction'
+
 export const FriendRequestActions = () => {
+    const { userName, userNo } = useParams()
+    const { data } = useSelector((state) => state.getUserProfileDetails)
+    const dispatch = useDispatch()
     return (
         <div className="flex items-center justify-end">
             <button
+                onClick={() =>
+                    dispatch(acceptFriendRequest(data?.data?._id, userName, userNo))
+                }
                 className="bg-green-500 active:bg-green-600 hover:bg-green-700 text-white font-bold text-xs px-4 py-2 rounded outline-none focus:outline-none mr-2 ease-linear transition-all duration-150"
                 type="button"
             >
-                Confirm Request
+                Accept Request
             </button>
             <button
                 className="bg-red-500 active:bg-red-600 hover:bg-red-700 text-white font-bold text-xs px-4 py-2 rounded outline-none focus:outline-none ease-linear transition-all duration-150"
