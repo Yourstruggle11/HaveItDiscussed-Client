67ee93f251962baa198236a36fc3f4ffe1b49d9c
Yourstruggle11 <sensouvik51@gmail.com>
2023-06-21 03:24:51 +0530

    `feat`: Complete connection for `remove friend` API

--- src/pages/Profile/components/Connection/components/FriendActions/index.jsx
+++ src/pages/Profile/components/Connection/components/FriendActions/index.jsx
@@ -1,4 +1,12 @@
+import { useParams } from 'react-router-dom'
+import { removeFriend as unFriend } from '../../../../../../redux/actions/friendsAction'
+import { useDispatch, useSelector } from 'react-redux'
+
 export const FriendActions = () => {
+    const { userName, userNo } = useParams()
+    const { data } = useSelector((state) => state.getUserProfileDetails)
+    const dispatch = useDispatch()
+
     return (
         <div className="flex items-center justify-end">
             <button
@@ -8,6 +16,7 @@ export const FriendActions = () => {
                 Chat
             </button>
             <button
+                onClick={() => dispatch(unFriend(data?.data?._id, userName, userNo))}
                 className="bg-red-500 active:bg-red-600 hover:bg-red-700 text-white font-bold text-xs px-4 py-2 rounded outline-none focus:outline-none ease-linear transition-all duration-150"
                 type="button"
             >
