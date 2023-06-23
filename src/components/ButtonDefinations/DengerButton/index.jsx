61f825ec7c4ea1678cdd4753a9217a8d26a0b4d3
Yourstruggle11 <sensouvik51@gmail.com>
2023-06-24 00:28:20 +0530

    `feat`: Added more `buttons` for `all category`

---
+++ src/components/ButtonDefinations/DengerButton/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const DengerButton = ({ children, onClick }) => {
+    return (
+        <>
+            <div
+                onClick={onClick && onClick}
+                class="relative cursor-pointer inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-red-500 rounded-xl group"
+            >
+                <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                    <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                </span>
+                <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
+                    {children}
+                </span>
+            </div>
+        </>
+    )
+}
--- src/pages/Profile/components/Connection/components/FriendActions/index.jsx
+++ src/pages/Profile/components/Connection/components/FriendActions/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { removeFriend as unFriend } from '../../../../../../redux/actions/friendsAction'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
+import { ButtonDefinations } from '../../../../../../components/ButtonDefinations'
 
 export const FriendActions = () => {
     const { userName, userNo } = useParams()
@@ -10,7 +11,7 @@ export const FriendActions = () => {
 
     return (
         <div className="flex items-center justify-end">
-            <button
+            <ButtonDefinations.PrimaryButton
                 onClick={() => {
                     toast("🦄 Shhh! This feature is not available yet. But don't worry this is going to be amazing.", {
                         position: 'top-right',
@@ -27,14 +28,12 @@ export const FriendActions = () => {
                 type="button"
             >
                 Chat
-            </button>
-            <button
+            </ButtonDefinations.PrimaryButton>
+            <ButtonDefinations.DengerButton
                 onClick={() => dispatch(unFriend(data?.data?._id, userName, userNo))}
-                className="bg-red-500 active:bg-red-600 hover:bg-red-700 text-white font-bold text-xs px-4 py-2 rounded outline-none focus:outline-none ease-linear transition-all duration-150"
-                type="button"
             >
                 Unfriend
-            </button>
+            </ButtonDefinations.DengerButton>
         </div>
     )
 }
